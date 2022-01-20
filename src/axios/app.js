0593daadb00d77c99bdb559838eb4fd610e4714f
yfh622 <yang_18435939203@163.com>
2022-01-20 13:51:47 +0800

    www去掉

--- src/axios/app.js
+++ src/axios/app.js
@@ -3,5 +3,5 @@ export const app = {
     // qms:"http://47.57.127.87:8080/"
     // qms:"http://8.210.19.71:5138/"
     // qms:"http://47.57.102.137:5138/"
-    qms:"http://www.cryptoglobal.cc:8088/"
+    qms:"http://cryptoglobal.cc:8088/"
 };
